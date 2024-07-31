a7f77822a7452a2c1afb1a25a7c69575502364be
SantiagoManchola <dmancholas@hotmail.com>
2024-07-31 14:22:53 -0500

    pages names changes

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,6 +10,16 @@ const routes = [
       name : 'register',
       component: () => import('../components/ss-register/SsRegister.vue'),
     },
+    {
+      path: '/signIn',
+      name : 'signIn',
+      component: () => import('../pages/auth/SsLogin.vue'),
+    },
+    {
+      path: '/companies',
+      name : 'companies',
+      component: () => import('../pages/auth/SsCompanies.vue'),
+    },
   
   ]
   
