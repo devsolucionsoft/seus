37e6f3207322619678ce3b289434fd17235ff362
SantiagoManchola <dmancholas@hotmail.com>
2024-07-31 12:44:50 -0500

    login desktop

---
+++ src/pages/auth/Empresas.vue
@@ -0,0 +1,14 @@
+<template>
+    <SsHeader></SsHeader>
+</template>
+
+<script>
+import SsHeader from '@/components/ss-header/SsHeader.vue';
+
+export default {
+  name: 'Empresas',
+  components: {
+    SsHeader
+  }
+};
+</script>
