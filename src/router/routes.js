c5e8a32e0872ece37c1d4d69aef86a717e1e356c
SantiagoManchola <dmancholas@hotmail.com>
2024-08-06 00:27:42 -0500

    form registration for desktop

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,5 @@
 const routes = [
     {
-      path: '/examples',
-      name : 'example',
-      component: () => import('../components/ss-example/SsExample.vue'),
-
-    },
-    {
       path: '/register',
       name : 'register',
       component: () => import('../components/ss-register/SsRegister.vue'),
