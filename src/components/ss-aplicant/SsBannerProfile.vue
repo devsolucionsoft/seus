df03c1f49824012dc194ca843514fe0b86fb4c05
SantiagoManchola <dmancholas@hotmail.com>
2024-08-19 14:37:28 -0500

    desktop basic information for person register

--- src/components/ss-aplicant/SsBannerProfile.vue
+++ src/components/ss-aplicant/SsBannerProfile.vue
@@ -18,6 +18,31 @@
               <span>Sube una imagen de perfil</span>
             </div>
           </div>
+          <div class="basic-info">
+            <div class="text">
+              <span>Bienvenido <br><b>Francisco José Benavides</b></span>
+              <div class="lineText"></div>
+            </div>
+            <div class="info">
+              <div class="element">
+                <span class="title">Email</span>
+                <span class="desc">FRJosé@gmail.com</span>
+              </div>
+              <div class="element">
+                <span class="title">Documento</span>
+                <div class="document-container desc">
+                  <span class="type">CC</span>
+                  <span class="#">#</span>
+                  <span class="desc">1022345789</span>
+                </div>
+              </div>
+              <div class="element">
+                <span class="title">Password</span>
+                <span class="desc">***********</span>
+              </div>
+            </div>
+            <button>Cambiar contraseña</button>
+          </div>
         </div>
     </section>
   </template>
@@ -76,6 +101,8 @@
       max-height: 112px
       object-fit: cover
       object-position: top
+      @media(min-width: 1200px)
+        max-height: 237px
 
     .edit-btn
       position: absolute
@@ -89,6 +116,9 @@
       cursor: pointer
       border-radius: 50px
       gap: 12px
+      @media(min-width: 1200px)
+        top: 47px
+        right: 184px
 
       .img-container
         height: 26px
@@ -114,6 +144,21 @@
     align-items: center
     justify-content: flex-start
     margin-left: 4%
+    gap: 33px
+    background-image: url('../../assets/images/bgLogin.jpeg')
+    background-position: center right
+    background-size: cover
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 100%)
+
+    @media(min-width: 1200px)
+      margin-top: -124px
+      margin-left: 196px
+      width: 100%
+      justify-content: center
+      gap: 33px
+      align-content: start
+      margin-left: 0
+      padding: 0 196px 40px 196px
 
     .profile-image
       position: relative
@@ -122,6 +167,9 @@
       border-radius: 50%
       overflow: hidden
       cursor: pointer
+      @media(min-width: 1200px)
+        width: 255px
+        height: 255px 
 
       img
         width: 100%
@@ -148,10 +196,15 @@
         justify-content: center
         flex-direction: column
         gap: 16px
-
+        @media(min-width: 1200px)
+          top: 30%
+          gap: 12px
         img
           max-width: 36px
           max-height: 33px
+          @media(min-width: 1200px)
+            max-width: 64px
+            max-height: 59px
         span
           font-size: 12px
           font-weight: 500
@@ -159,5 +212,78 @@
           text-align: center
           color: #47586E
           max-width: 80%
+          @media(min-width: 1200px)
+            font-size: 16px
+            line-height: 24px
+    .basic-info
+      display: none
+      margin-top: 142px
+      flex-direction: column
+      align-items: start
+      gap: 23px
+      width: 70%
+
+      .text
+        display: flex
+        flex-direction: column
+        gap: 15px
+        span
+          font-size: 24px
+          font-weight: 500
+          line-height: 29.26px
+          text-align: left
+          color: #023D6A
+          b
+            font-size: 34px
+            font-weight: 700
+            line-height: 41.45px
+        .lineText
+          max-width: 64px
+          width: 100%
+          border-radius: 7px
+          height: 8px
+          background-color: #761D74
+          border: none
+      .info
+        display: flex
+        flex-direction: row
+        width: 100%
+        justify-content: space-between
+        padding-right: 67px
+        .element
+          display: flex
+          flex-direction: column
+          gap: 18px
+          .title
+            font-size: 14px
+            font-weight: 500
+            line-height: 17.07px
+            text-align: left
+            color: #9E9E9E
+          .desc
+            font-size: 16px
+            font-weight: 500
+            line-height: 19.5px
+            text-align: left
+            align-items: center
+          .document-container
+            display: flex
+            flex-direction: row
+            gap: 11px
+      button
+        padding: 9px 11px 
+        background-color: #D5B9D4
+        border-radius: 28px
+        font-size: 12px
+        font-weight: 500
+        line-height: 14.63px
+        text-align: center
+        color: #290A29
+        align-self: flex-end
+
+      @media(min-width: 1200px)
+        display: flex
+
+
 </style>
   
