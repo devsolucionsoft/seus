0eb86d4e0dc86bb9dcd6b29970e2d396daa6980f
wilintonascanio <wiliascanio1208@gmail.com>
2024-10-02 10:48:18 -0500

    add cors config

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,16 +1,9 @@
-// src/plugins/axios.js
-import axios from 'axios';
+import axios from "axios";
 
-export const api = axios.create({
-  baseURL: 'http://3.233.173.228:5003/',
-  timeout: 5000,
+const api = axios.create({
+  baseURL: "/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
-
-export default {
-  install: (app) => {
-    app.config.globalProperties.$axios = api;
-  },
-};
+export { api };
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     devServer: {
-        proxy: {
-            '/v1': {
-                target: 'http://3.233.173.228:5003/',
-                changeOrigin: true,
-                pathRewrite: { '^/v1': '' },
-            }
-        }
-    }
-};
+      proxy: {
+        '^/api': {
+          target: 'http://3.233.173.228:5003/',
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' },
+        },
+      },
+    },
+  };
   
