c7202017a820149bc4230a74e3940d5c7a97c513
Santiago Manchola Serna <dmancholas@hotmail.com>
2024-10-03 08:07:31 -0500

    set axios connection for production deploy url

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        '^/api': {
-          target: 'http://3.233.173.228:5003/',
-          changeOrigin: true,
-          pathRewrite: { '^/api': '' },
-        },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://3.233.173.228:5003/',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
       },
     },
-  };
-  
+  },
+};
