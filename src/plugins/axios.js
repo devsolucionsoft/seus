2887c5789ab809ad37b20fa3395c6ad3a0c74b70
SantiagoManchola <dmancholas@hotmail.com>
2024-10-04 08:15:51 -0500

    axio.js revert

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://seusapi.solucionsoft.com/' // Backend url config for production
-    : '/api', // Dev is config by proxy
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 export { api };
